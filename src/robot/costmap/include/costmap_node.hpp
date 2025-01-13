734eea65d873f278a8dbe0d58919f612964ff7b2
cilo chou <zmh1853522906@gmail.com>
2025-01-13 01:04:55 -0500

    Update costmap_node.hpp

--- src/robot/costmap/include/costmap_node.hpp
+++ src/robot/costmap/include/costmap_node.hpp
@@ -1,23 +1,31 @@
 #ifndef COSTMAP_NODE_HPP_
 #define COSTMAP_NODE_HPP_
- 
+
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
- 
+#include "sensor_msgs/msg/laser_scan.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
+
 #include "costmap_core.hpp"
- 
+
 class CostmapNode : public rclcpp::Node {
   public:
     CostmapNode();
     
-    // Place callback function here
     void publishMessage();
- 
+    
+    void handleLaserScan(const sensor_msgs::msg::LaserScan::SharedPtr msg) const;
+
   private:
-    robot::CostmapCore costmap_;
-    // Place these constructs here
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr string_pub_;
-    rclcpp::TimerBase::SharedPtr timer_;
+    robot::CostmapCore costmap_processor_;
+    std::string scan_topic_;
+    std::string map_topic_;
+    double map_resolution_;
+    int map_width_;
+    int map_height_;
+    geometry_msgs::msg::Pose map_origin_;
+    double cost_inflation_radius_;
+    rclcpp::Subscription<sensor_msgs::msg::LaserScan>::SharedPtr laser_scan_subscription_;
+    rclcpp::Publisher<nav_msgs::msg::OccupancyGrid>::SharedPtr costmap_publisher_;
 };
- 
-#endif 
+
+#endif
