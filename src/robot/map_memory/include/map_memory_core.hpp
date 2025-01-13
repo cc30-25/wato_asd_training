491fbaa3cec83b1d9bac8a7d1f1ccd6ba9afe8c9
cilo chou <zmh1853522906@gmail.com>
2025-01-13 00:44:06 -0500

    Update map_memory_core.hpp

--- src/robot/map_memory/include/map_memory_core.hpp
+++ src/robot/map_memory/include/map_memory_core.hpp
@@ -1,19 +1,37 @@
-#ifndef MAP_MEMORY_CORE_HPP_
-#define MAP_MEMORY_CORE_HPP_
+#ifndef MAP_MEMORY_HANDLER_HPP_
+#define MAP_MEMORY_HANDLER_HPP_
 
 #include "rclcpp/rclcpp.hpp"
+#include "nav_msgs/msg/occupancy_grid.hpp"
 
 namespace robot
 {
-
 class MapMemoryCore {
-  public:
+ public:
     explicit MapMemoryCore(const rclcpp::Logger& logger);
 
-  private:
+    void configureMapMemory(
+        double resolution, 
+        int grid_width, 
+        int grid_height, 
+        const geometry_msgs::msg::Pose& origin_pose
+    );
+
+    void refreshMap(
+        const nav_msgs::msg::OccupancyGrid::SharedPtr& local_map,
+        double x_position, double y_position, double orientation
+    );
+
+    bool convertToMapCoordinates(
+        double robot_x, double robot_y, int& map_x, int& map_y
+    ) const;
+
+    nav_msgs::msg::OccupancyGrid::SharedPtr retrieveMap() const;
+
+ private:
+    nav_msgs::msg::OccupancyGrid::SharedPtr global_map_;
     rclcpp::Logger logger_;
 };
-
 }  
 
-#endif  
+#endif  // MAP_MEMORY_HANDLER_HPP_
